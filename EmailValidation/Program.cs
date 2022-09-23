d15e2eda26388ed87939b8f2340bf9a070bbd6d5
surajbridgelabz <surajnsinha@gmail.com>
2022-09-23 23:01:20 +0530

    [suraj] Add:To validate Email

--- EmailValidation/Program.cs
+++ EmailValidation/Program.cs
@@ -8,6 +8,22 @@
             Console.WriteLine("Enter Email: ");
             string email = Console.ReadLine();
             emailsample.ValidationEmail(email);
+
+            Console.WriteLine("Enter Email: ");
+            string email1 = Console.ReadLine();
+            emailsample.ValidationEmail1(email1);
+
+            Console.WriteLine("Enter Email: ");
+            string email2 = Console.ReadLine();
+            emailsample.ValidationEmail2(email2);
+
+            Console.WriteLine("Enter Email: ");
+            string email3 = Console.ReadLine();
+            emailsample.ValidationEmail3(email3);
+
+            Console.WriteLine("Enter Email: ");
+            string email4 = Console.ReadLine();
+            emailsample.InValidationEmail(email4);
         }
     }
 }
--- EmailValidation/ValidationEmailSample.cs
+++ EmailValidation/ValidationEmailSample.cs
@@ -16,5 +16,33 @@ namespace EmailValidation
             bool result = regex.IsMatch(email);
             Console.WriteLine(result);
         }
+        public const string VALIDEMAIL_REGEX = "^[a-z]+[.][0-9]+[@][a-zA-z]+[.][a-zA-Z]{3}$";
+        public void ValidationEmail1(string email1)
+        {
+            Regex regex = new Regex(VALIDEMAIL_REGEX);
+            bool result = regex.IsMatch(email1);
+            Console.WriteLine(result);
+        }
+        public const string VALIDEMAIL1_REGEX = "^[a-z]+[-][0-9]+[@][a-zA-z]+[.][a-zA-Z]{3}$";
+        public void ValidationEmail2(string email2)
+        {
+            Regex regex = new Regex(VALIDEMAIL1_REGEX);
+            bool result = regex.IsMatch(email2);
+            Console.WriteLine(result);
+        }
+        public const string VALIDEMAIL2_REGEX = "^[a-z]+[0-9]+[@][a-zA-z]+[.][a-zA-Z]{3}$";
+        public void ValidationEmail3(string email3)
+        {
+            Regex regex = new Regex(VALIDEMAIL2_REGEX);
+            bool result = regex.IsMatch(email3);
+            Console.WriteLine(result);
+        }
+        public const string INVALIDEMAIL_REGEX = "^[a-zA-Z]$";
+        public void InValidationEmail(string email4)
+        {
+            Regex regex = new Regex(INVALIDEMAIL_REGEX);
+            bool result = regex.IsMatch(email4);
+            Console.WriteLine(result);
+        }
     }
 }
