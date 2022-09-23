411ae198f17b7c45ae0d79c00932aa676fc5522e
surajbridgelabz <surajnsinha@gmail.com>
2022-09-23 23:00:11 +0530

    [suraj] Add:To validate Email

---
+++ EmailValidation/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace EmailValidation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidationEmailSample emailsample = new ValidationEmailSample();
+            Console.WriteLine("Enter Email: ");
+            string email = Console.ReadLine();
+            emailsample.ValidationEmail(email);
+        }
+    }
+}
---
+++ EmailValidation/ValidationEmailSample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EmailValidation
+{
+    public class ValidationEmailSample
+    {
+        public const string EMAIL_REGEX = "^[a-z]+[@][a-zA-z]+[.][a-zA-Z]{3}$";
+        public void ValidationEmail(string email)
+        {
+            Regex regex = new Regex(EMAIL_REGEX);
+            bool result = regex.IsMatch(email);
+            Console.WriteLine(result);
+        }
+    }
+}
